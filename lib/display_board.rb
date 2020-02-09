c48a420444d0e209a7c71e5017ad5dd1771dbdea
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-09 02:02:16 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 #display_board method prints a 3x3 tic tac toe board
 def display_board
-  puts "   |   |   |"
-  puts  "-----------"
-  puts "   |   |   |"
+  puts "   |   |   "
   puts "-----------"
-  puts "   |   |   |"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
 
